2b0fee7ce271a73f20352153764d5742da66680f
Stroe Rares <stroerares0@gmail.com>
2024-05-07 02:33:56 +0300

    Added Admin dashboard

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Http\Controllers\Controller;
+use App\Models\Order;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 
@@ -13,9 +14,55 @@ class AdminController extends Controller
     public function index()
     {
         $user = Auth::user();
+        $orders = Order::all();
+        $monthly_revenue = $orders->where('created_at', '>=', now()->subMonth())->sum('total_price');
+        $total_revenue = $orders->sum('total_price');
+        $total_orders = $orders->count();
+        $monthly_orders = $orders->where('created_at', '>=', now()->subMonth())->count();
+        $total_customers = $orders->groupBy('user_id')->count();
+        $monthly_customers = $orders->where('created_at', '>=', now()->subMonth())->groupBy('user_id')->count();
 
-        // Log::info($user);
+        $county_counts = [];
+        $city_counts = [];
 
-        return Inertia::render('Admin/AdminDashboard');
+        foreach ($orders as $order) {
+            $shipping_address = $order->shipping_address;
+
+            $address_parts = explode(";", $shipping_address);
+
+            $county = $address_parts[1];
+            $city = $address_parts[2];
+
+            if (isset($city_counts[$city])) {
+                $city_counts[$city]++;
+            } else {
+                $city_counts[$city] = 1;
+            }
+
+            if (isset($county_counts[$county])) {
+                $county_counts[$county]++;
+            } else {
+                $county_counts[$county] = 1;
+            }
+        }
+
+        arsort($city_counts);
+        $most_popular_city = key($city_counts);
+
+        arsort($county_counts);
+        $most_popular_county = key($county_counts);
+
+        return Inertia::render('Admin/AdminDashboard', [
+            'user' => $user,
+            'orders' => $orders,
+            'monthly_revenue' => $monthly_revenue,
+            'total_revenue' => $total_revenue,
+            'total_orders' => $total_orders,
+            'monthly_orders' => $monthly_orders,
+            'total_customers' => $total_customers,
+            'monthly_customers' => $monthly_customers,
+            'most_popular_city' => $most_popular_city,
+            'most_popular_county' => $most_popular_county,
+        ]);
     }
 }
--- resources/js/Pages/Admin/AdminDashboard.jsx
+++ resources/js/Pages/Admin/AdminDashboard.jsx
@@ -1,11 +1,56 @@
 import React from "react";
 import AdminHeader from "../../Components/Admin/AdminHeader";
+import Content from "../../Components/Admin/Content";
 
-export default function AdminDashboard() {
+export default function AdminDashboard({
+    orders,
+    monthly_revenue,
+    total_revenue,
+    total_orders,
+    monthly_orders,
+    total_customers,
+    monthly_customers,
+    most_popular_city,
+    most_popular_county,
+}) {
     return (
         <div>
             <AdminHeader title="Acasa">
-                <p>test</p>
+                <Content>
+                    <h3>Incasari Lunare</h3>
+                    <p className="text-dashboard-admin">
+                        {monthly_revenue} RON
+                    </p>
+                </Content>
+                <Content>
+                    <h3>Incasari Totale</h3>
+                    <p className="text-dashboard-admin">{total_revenue} RON</p>
+                </Content>
+                <Content>
+                    <h3>Cel mai popular oras si Judet</h3>
+                    <p className="text-dashboard-admin">
+                        Oras: {most_popular_city}
+                    </p>
+                    <p className="text-dashboard-admin">
+                        Judet: {most_popular_county}
+                    </p>
+                </Content>
+                <Content>
+                    <h3>Numar comenzi lunare</h3>
+                    <p className="text-dashboard-admin">{total_orders}</p>
+                </Content>
+                <Content>
+                    <h3>Numar comenzi totale</h3>
+                    <p className="text-dashboard-admin">{monthly_orders}</p>
+                </Content>
+                <Content>
+                    <h3>Numar clienti lunari</h3>
+                    <p className="text-dashboard-admin">{monthly_customers}</p>
+                </Content>
+                <Content>
+                    <h3>Numar clienti</h3>
+                    <p className="text-dashboard-admin">{total_customers}</p>
+                </Content>
             </AdminHeader>
         </div>
     );
